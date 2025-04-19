4147ee3359e7fecc059131d64508b9d081927458
Littl3Fox <102700820+Littl3Fox@users.noreply.github.com>
2025-04-19 17:53:11 -0300

    Adicionado função que confere o valor e função que faz requisição a API e finalização da main

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
+import utils
 
 def main():
-   
+
+    #Aqui e recebo do usuário a moeda de origem(ori) e destino(dest)
+
+    ori = str(input("Digite a moeda de origem: "))
+    dest = str(input("Digite a moeda de destino:"))
+
+    #Coloco numa lista para análise
+    moedas = [ori,dest]
+
+    #Vejo se as moedas existem de fato
+    try:
+        for moeda in moedas:
+            utils.confere_moedas(moeda)
+    except ValueError as error:
+        print(error)
+        return
+    
+    #Recebo o valor a ser convertido
+    valor = (float(input("Digite o valor a ser convertido: ")))
+
+    #Verifico se o valor é válido
+    try:
+        utils.confere_valor(valor)
+    except ValueError as error:
+        print(error)
+        return
+
+    #Converto a moeda
+
+    try:
+        resultado = utils.converte_moedas(moedas,valor)
+        #Mostra o resultado
+        print(f"O valor de {valor} de {moedas[0]} equivale a {resultado} de {moedas[1]}")
+    except Exception as error:
+        print(error)
+        return
+    
     return 0
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import xml.etree.ElementTree as ET
+import requests
+#Confere se a moeda é válida e existe.
 
 def confere_moedas(simbolo):
 
@@ -25,3 +27,33 @@ def confere_moedas(simbolo):
         return 0
     else:
         raise ValueError(f"Simbolo {simbolo} de moeda invalida")
+    
+#Confere se o valor passado é válido
+
+def confere_valor(valor):
+
+    if valor <= 0:
+        raise ValueError("O valor digitado é inválido!")
+    
+#Converte as moedas
+#Recebe uma lista moedas com a moeda de origem(moedas[0]) e moeda de destino(moeda[1])
+#Recebe também o valor a ser convertido
+
+def converte_moedas(moedas,valor):
+
+    #Aqui eu faço uma requisição dos dados para a API
+    resposta = requests.get(f"https://economia.awesomeapi.com.br/json/last/{moedas[0]}-{moedas[1]}")
+
+    #Verifico se a requisição funcionou (200) e tive uma resposta favorável.
+    if resposta.status_code == 200:
+        #Aqui pego os dados da requisição.
+        dados = resposta.json()
+
+        #Pego o valor da moeda de destino 
+        valor_dest = dados[moedas[0]+moedas[1]]['bid']
+
+        #Retorno o valor convertido
+        return valor * float(valor_dest) 
+    else:
+
+        raise ValueError("Requisição a API falhou")
